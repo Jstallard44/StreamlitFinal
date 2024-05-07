8908028a85d906b6bfdef1ec4908d237401d6392
Jstallard44 <jstalla1@uncc.edu>
2024-05-06 22:09:57 -0400

    Removed requirements

--- Final.py
+++ Final.py
@@ -1,7 +1,4 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import matplotlib.pyplot as plt, matplotlib
-import plotly as py
+import pandas as pd 
 import plotly.graph_objs as go
 import streamlit as st
 
